53aa216caa3c0de610ec7dbdb9d7ec4dedf0d538
rmpestano <bigboss666>
2016-02-20 13:06:58 -0200

    refs #3

--- cukedoctor-converter/src/main/java/com/github/cukedoctor/i18n/I18nLoader.java
+++ cukedoctor-converter/src/main/java/com/github/cukedoctor/i18n/I18nLoader.java
@@ -2,44 +2,43 @@ package com.github.cukedoctor.i18n;
 
 import com.github.cukedoctor.api.model.Feature;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.*;
+import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static com.github.cukedoctor.util.Assert.hasText;
 
 /**
  * This class is responsible for internationalization.
- *
+ * <p>
  * It uses features language to determine in which language the documentation will be generated.
- *
+ * </p>
  * Created by pestano on 19/02/16.
- *
  */
 public class I18nLoader {
 
     private static I18nLoader instance;
+    private static Logger log = Logger.getLogger(I18nLoader.class.getName());
 
     private ResourceBundle bundle;
 
-    private I18nLoader(){
+
+    private I18nLoader() {
 
     }
 
-    public static synchronized I18nLoader instance(List<Feature> features){
-        if (instance == null){
+    public static synchronized I18nLoader instance(List<Feature> features) {
+        if (instance == null) {
             instance = new I18nLoader();
             instance.init(features);
         }
         return instance;
     }
 
-    public static synchronized I18nLoader newInstance(List<Feature> features){
+    public static synchronized I18nLoader newInstance(List<Feature> features) {
         instance = null;
         return instance(features);
     }
@@ -58,22 +57,22 @@ public class I18nLoader {
 
     private void init(List<Feature> features) {
         String lang = resolveFeatureLanguage(features);
-        if(!hasText(lang)){
+        if (!hasText(lang)) {
             lang = "en";
         }
-        if(lang != null){
-            try{
-                bundle = ResourceBundle.getBundle("cukedoctor", Locale.forLanguageTag(lang),new UTF8Control());
-            }catch (MissingResourceException e){
-                Logger.getLogger(I18nLoader.class.getName()).warning(String.format("No resource bundle found for language %s. Using 'cukedoctor_en.properties' as default bundle.",lang));
+        if (lang != null) {
+            try {
+                bundle = ResourceBundle.getBundle("cukedoctor", Locale.forLanguageTag(lang), new UTF8Control());
+            } catch (MissingResourceException e) {
+                log.warning(String.format("No resource bundle found for language %s. Using 'cukedoctor_en.properties' as default bundle.", lang));
             }
         }
     }
 
     private String resolveFeatureLanguage(List<Feature> features) {
-        if (features != null){
+        if (features != null) {
             for (Feature feature : features) {
-                if(hasText(feature.getLanguage())){
+                if (hasText(feature.getLanguage())) {
                     return feature.getLanguage();
                 }
             }
@@ -84,25 +83,16 @@ public class I18nLoader {
     public static class UTF8Control extends ResourceBundle.Control {
         public java.util.ResourceBundle newBundle
                 (String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
-                throws IllegalAccessException, InstantiationException, IOException
-        {
+                throws IllegalAccessException, InstantiationException, IOException {
             // The below code is copied from default Control#newBundle() implementation.
             // Only the PropertyResourceBundle line is changed to read the file as UTF-8.
 
             String bundleName = toBundleName(baseName, locale);
             String resourceName = toResourceName(bundleName, "properties");
             java.util.ResourceBundle bundle = null;
-            InputStream stream = null;
-            if (reload) {
-                URL url = loader.getResource(resourceName);
-                if (url != null) {
-                    URLConnection connection = url.openConnection();
-                    if (connection != null) {
-                        connection.setUseCaches(false);
-                        stream = connection.getInputStream();
-                    }
-                }
-            } else {
+            //client applications may provide a custom bundle
+            InputStream stream = getBundleFromTargetTestFolder();
+            if (stream == null) {
                 stream = loader.getResourceAsStream(resourceName);
             }
             if (stream != null) {
@@ -114,6 +104,27 @@ public class I18nLoader {
             }
             return bundle;
         }
-    }
 
+        /**
+         * looks for a file named cukedoctor.properties in target/test-classes
+         *
+         * client applications just need to have src/test/resources/cukedoctor.properties
+         *
+         * @return
+         */
+        private InputStream getBundleFromTargetTestFolder() {
+            String resourcePath = (Paths.get("").toAbsolutePath() + "/target/test-classes/cukedoctor.properties").replaceAll("//", "/");
+            File file = new File(resourcePath);
+            if (file.exists()) {
+                log.fine("Loading cukedoctor resource bundle from: " + resourcePath);
+                try {
+                    return new FileInputStream(file);
+                } catch (Exception e) {
+                    log.log(Level.WARNING, "Could not load resource bundle from target folder", e);
+                }
+            }
+            return null;
+        }
+
+    }
 }
