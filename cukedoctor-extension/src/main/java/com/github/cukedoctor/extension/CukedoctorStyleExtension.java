ba31f4604ae33cb6ba6e632095f4b942fae20b9d
rmpestano <rmpestano@gmail.com>
2016-09-04 20:21:33 -0300

    avoid horizontal bar in summary section

--- cukedoctor-extension/src/main/java/com/github/cukedoctor/extension/CukedoctorStyleExtension.java
+++ cukedoctor-extension/src/main/java/com/github/cukedoctor/extension/CukedoctorStyleExtension.java
@@ -21,30 +21,26 @@ public class CukedoctorStyleExtension extends Postprocessor{
 
     @Override
     public String process(Document document, String output) {
-        if(document.basebackend("html") ){
+        if (document.basebackend("html")) {
             org.jsoup.nodes.Document doc = Jsoup.parse(output, "UTF-8");
 
             Element contentElement = doc.getElementById("footer");
-                if(System.getProperty(STYLE_DISABLE_EXT_KEY) == null) {
-                        addStyleClass(contentElement);
-                }
+            if (System.getProperty(STYLE_DISABLE_EXT_KEY) == null) {
+                addStyleClass(contentElement);
+            }
 
             return doc.html();
 
-        }else{
+        } else {
             return output;
         }
     }
 
-        private void addStyleClass(Element contentElement) {
-             String styleClass = " <style>  \n" +
-                     "\n" +
-                     ".sidebarblock, .sectionbody, .content{\n" +
-                 "overflow:auto!important;\n" +
-                 "}\n";
+    private void addStyleClass(Element contentElement) {
+        String styleClass = " <style>  \n" + "\n" + "#content:padding:0!important;\n" + ".sidebarblock, .sectionbody, .content{\n" + "overflow:auto!important;\n" + "}\n";
 
-                contentElement.after(styleClass);
-        }
+        contentElement.after(styleClass);
+    }
 
 
 }
