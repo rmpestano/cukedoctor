3f5677d52560396c2d7efb0599c8eb0f8761cda4
Andrew Sweet <andrewesweet@users.noreply.github.com>
2021-04-10 10:10:32 +0100

    Tests to assert that null-valued attributes are not included in the attribute map and hence not passed on the "command line" to asciidoctor

---
+++ cukedoctor-converter/src/test/java/com/github/cukedoctor/api/DocumentAttributesTest.java
@@ -0,0 +1,34 @@
+package com.github.cukedoctor.api;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Map;
+
+import static com.github.cukedoctor.util.Constants.Attributes.Name.LINKCSS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnit4.class)
+public class DocumentAttributesTest {
+    @Test
+    public void toMapMustNotIncludeAttributesWithNullValues() {
+        DocumentAttributes attributes = new DocumentAttributes();
+        attributes.setLinkCss(null);
+        Assert.assertNull(attributes.isLinkCss());
+
+        Map<String, Object> result = attributes.toMap();
+        assertThat(result).doesNotContainKey(LINKCSS.getName());
+    }
+
+    @Test
+    public void toMapMustIncludeAttributesWithNonNullValues() {
+        DocumentAttributes attributes = new DocumentAttributes();
+        attributes.setLinkCss(false);
+        Assert.assertFalse(attributes.isLinkCss());
+
+        Map<String, Object> result = attributes.toMap();
+        assertThat(result).containsKey(LINKCSS.getName());
+    }
+}
